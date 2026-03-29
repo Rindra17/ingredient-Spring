f1205c69074dd46e8cd16d3275eccc336c9e525b
rindraniaina <rindraniainarasolofondrasoa17@gmail.com>
2026-03-29 23:37:24 +0300

    refactor: use DataSourcee instead of jdbcTemplate

--- src/main/java/hei/school/ingredient/repository/IngredientRepository.java
+++ src/main/java/hei/school/ingredient/repository/IngredientRepository.java
@@ -2,29 +2,53 @@ package hei.school.ingredient.repository;
 
 import hei.school.ingredient.entity.CategoryEnum;
 import hei.school.ingredient.entity.Ingredient;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class IngredientRepository {
-    private JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
 
-    public IngredientRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public IngredientRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
-    public List<Ingredient> findAll() {
-        String sql = "select id, name, category, price from ingredient";
+    public List<Ingredient> findAll(int page, int size) {
+        String sql = """
+                select id, name, category, price
+                from ingredient
+                order by id
+                limit ? offset ?
+                """;
+        List<Ingredient> ingredients = new ArrayList<>();
+        int offset = (page - 1) * size;
 
-        return jdbcTemplate.query(sql, (rs, rowNum) -> {
-            Ingredient ingredient = new Ingredient();
-            ingredient.setId(rs.getInt("id"));
-            ingredient.setName(rs.getString("name"));
-            ingredient.setCategory(CategoryEnum.valueOf(rs.getString("category")));
-            ingredient.setPrice(rs.getDouble("price"));
-            return ingredient;
-        });
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement stmt = connection.prepareStatement(sql);) {
+                stmt.setInt(1, size);
+                stmt.setInt(2, offset);
+                try (ResultSet rs = stmt.executeQuery()) {
+                    while(rs.next()) {
+                        Ingredient ingredient = new Ingredient();
+                        ingredient.setId(rs.getInt("id"));
+                        ingredient.setName(rs.getString("name"));
+                        ingredient.setCategory(CategoryEnum.valueOf(rs.getString("category")));
+                        ingredient.setPrice(rs.getDouble("price"));
+
+                        ingredients.add(ingredient);
+                    }
+                    return ingredients;
+                }
+        }
+        catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 }
